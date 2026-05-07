bae497d64fa2540f842da476705093640f3b5a44
Sven Maier <svenmaier@Svens-MacBook-Pro.local>
2026-05-07 17:51:14 +0200

    initial scaffold with generated Dataverse services

---
+++ .power/schemas/appschemas/dataSourcesInfo.ts
@@ -0,0 +1,43 @@
+/*!
+ * Copyright (C) Microsoft Corporation. All rights reserved.
+ * This file is auto-generated. Do not modify it manually.
+ * Changes to this file may be overwritten.
+ */
+
+export const dataSourcesInfo = {
+  "cr9b5_pt_attachments": {
+    "tableId": "",
+    "version": "",
+    "primaryKey": "cr9b5_pt_attachmentid",
+    "dataSourceType": "Dataverse",
+    "apis": {}
+  },
+  "cr9b5_pt_contacts": {
+    "tableId": "",
+    "version": "",
+    "primaryKey": "cr9b5_pt_contactid",
+    "dataSourceType": "Dataverse",
+    "apis": {}
+  },
+  "cr9b5_pt_invoices": {
+    "tableId": "",
+    "version": "",
+    "primaryKey": "cr9b5_pt_invoiceid",
+    "dataSourceType": "Dataverse",
+    "apis": {}
+  },
+  "cr9b5_pt_properties": {
+    "tableId": "",
+    "version": "",
+    "primaryKey": "cr9b5_pt_propertyid",
+    "dataSourceType": "Dataverse",
+    "apis": {}
+  },
+  "cr9b5_pt_references": {
+    "tableId": "",
+    "version": "",
+    "primaryKey": "cr9b5_pt_referenceid",
+    "dataSourceType": "Dataverse",
+    "apis": {}
+  }
+};
